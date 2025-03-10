7a00a6534483d895fafdfb40e3270cf69c178953
= <=>
2025-03-10 16:18:57 +0200

    done

--- script.js
+++ script.js
@@ -1,35 +1,66 @@
 let submitBtn = document.getElementById("submit-btn");
+let section = document.getElementById("country-info");
+let sectionNeigh = document.getElementById("bordering-countries");
+
+
 submitBtn.addEventListener("click", async ()=>{
     let countryName = document.getElementById("country-name").value;
     if(countryName){
         let data = await getCountryData(countryName);
-        createCountryInfo(data);
+        if(data.status === 404){
+            alert("Country not found");
+            section.innerHTML = "";
+            let errorP = document.createElement("p");
+            errorP.innerText = "Country not found";
+            errorP.style.color = "red";
+            section.appendChild(errorP);
+            return;
+        }else{
+            createCountryInfo(data);
+        }
         let neighbours = await getNeighbours(data);
-        creteNeighbours(neighbours);
+        if(Object.keys(neighbours).length === 0){
+            sectionNeigh.innerHTML = "";
+            let noNeighbours = document.createElement("h2");
+            noNeighbours.innerText = "No bordering countries";
+            noNeighbours.style.color = "red";
+            section.appendChild(noNeighbours);
+            return;
+        }else{
+            creteNeighbours(neighbours);
+        }
 
     }else{
         alert("Please enter a country name");
+        section.innerHTML = "";
+        let errorP = document.createElement("p");
+        errorP.innerText = "Please enter a country name";
+        errorP.style.color = "red";
+        section.appendChild(errorP);
     }
 });
 
 async function getCountryData(countryName) {
     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
     const data = await response.json();
+    if(data.status === 404){
+        return data;
+    }else{
 
-    const countryData = {
-        name: data[0].name.common,
-        capital: data[0].capital ? data[0].capital[0] : 'N/A',
-        population: data[0].population,
-        region: data[0].region,
-        flag: data[0].flags.png,
-        borders: data[0].borders
-    };
+        const countryData = {
+            name: data[0].name.common,
+            capital: data[0].capital ? data[0].capital[0] : 'N/A',
+            population: data[0].population,
+            region: data[0].region,
+            flag: data[0].flags.png,
+            borders: data[0].borders? data[0].borders : []
+        };
+        return countryData;
+    }
 
-    return countryData;
 }
 
 function createCountryInfo(countryData) {
-    let section = document.getElementById("country-info");
     section.innerHTML = "";
     let h1 = document.createElement("h1");
     h1.innerText = `${countryData.name}`;
@@ -51,6 +82,9 @@ function createCountryInfo(countryData) {
 
 async function getNeighbours(countryData){
     let codes = countryData.borders;
+    if(codes.length === 0){
+        return {};
+    }
     let neighbours = {};
     for(let code of codes){
         let response = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
@@ -64,11 +98,11 @@ async function getNeighbours(countryData){
 }
 
 function creteNeighbours(neighbours){
-    let section = document.getElementById("bordering-countries");
-    section.innerHTML = "";
+    
+    sectionNeigh.innerHTML = "";
     let h1 = document.createElement("h1");
     h1.innerText = `Bordering Countries`;
-    section.appendChild(h1);
+    sectionNeigh.appendChild(h1);
     let ul = document.createElement("ul");
     ul.classList.add("list");
     for(let neighbour in neighbours){
@@ -80,7 +114,7 @@ function creteNeighbours(neighbours){
         neighbourLi.appendChild(img);
         ul.appendChild(neighbourLi);
     }
-    section.appendChild(ul);
+    sectionNeigh.appendChild(ul);
 
 
 
